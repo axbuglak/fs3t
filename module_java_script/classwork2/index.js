5e5bbd105a9ed97b7065377f21f541bd3747ef90
Alexey-Buglak <83399637+Alexey-Buglak@users.noreply.github.com>
2021-05-03 22:56:00 +0300

    add classwork2

---
+++ module_java_script/classwork2/index.js
@@ -0,0 +1,47 @@
+// console.log(0xff);
+
+// console.log(1 == '1');
+// const randomNumber = Math.floor(Math.random() * 11);
+// const prof = prompt('youre profeshion');
+
+// if (true && false ) {
+// 	console.log('work');
+// } else {
+// 	console.log('not work');
+// }
+// if (prof === 'Director') {
+// 	console.log('dir');
+// } else if (prof === 'Buhalter') {
+// 	console.log('buh');
+// } else if (prof === 'Sekretar') {
+// 	console.log('sekretar');
+// }
+
+// switch (prof) {
+// 	case 'Director':
+// 		console.log('dir');
+// 		break;
+// 	case 'Buhalter':
+// 		console.log('buh');
+// 		break;
+// 	case 'Sekretar':
+// 		console.log('sekretar');
+// }
+
+// console.log(randomNumber);
+// if (age < 18) {
+// 	console.log('ne proshel');
+// } else if (age < 22 || age < 18) {
+// 	let answer = confirm('хочешь ли ты продолжить');
+// 	if (answer) {
+// 		console.log('welcome');
+// 	} else {
+// 		console.log('exit');
+// 	}
+// } else {
+// 	console.log('welcome');
+// }
+
+// console.log(Math.floor(5.9)); // в большую
+// console.log(Math.ceil(5.1)); // в меньшую сторону
+
